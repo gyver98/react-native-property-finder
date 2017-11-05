42cd891ffe8254b466e586414c719df503d0485d
somdari <matthewchoi7@gmail.com>
2017-11-05 22:31:24 +1100

    adding SearchResults component

--- SearchPage.js
+++ SearchPage.js
@@ -8,25 +8,26 @@ import {
   ActivityIndicator,
   Image,
 } from 'react-native';
-import SearchResults from './SearchResults';
 
-function urlForQueryAndPage(key, value, pageNumber) {
-  const data = {
-    country: 'uk',
-    pretty: '1',
-    encoding: 'json',
-    listing_type: 'buy',
-    action: 'search_listings',
-    page: pageNumber
-  };
-  data[key] = value;
-  
-  const querystring = Object.keys(data)
-    .map(key => key + '=' + encodeURIComponent(data[key]))
-    .join('&');
+import SearchResults from './SearchResults'; // ERROR. The Expo team has been notified.
+
+  function urlForQueryAndPage(key, value, pageNumber) {
+    const data = {
+      country: 'uk',
+      pretty: '1',
+      encoding: 'json',
+      listing_type: 'buy',
+      action: 'search_listings',
+      page: pageNumber
+    };
+    data[key] = value;
     
-  return 'https://api.nestoria.co.uk/api?' + querystring;
-}
+    const querystring = Object.keys(data)
+      .map(key => key + '=' + encodeURIComponent(data[key]))
+      .join('&');
+      
+    return 'https://api.nestoria.co.uk/api?' + querystring;
+  }
 
 export default class SearchPage extends Component {
   constructor(props) {
@@ -37,6 +38,14 @@ export default class SearchPage extends Component {
       message: ''
     };
   }
+  
+  _onSearchTextChanged = (event) => {
+    console.log('on search text changed');
+    this.setState({ 
+      searchString: event.nativeEvent.text 
+    });
+    console.log('Current: '+ this.state.searchString+', Next: '+event.nativeEvent.text)
+  };
   
   _handleResponse = (response) => {
     this.setState({
@@ -53,14 +62,6 @@ export default class SearchPage extends Component {
       this.setState({ message: 'Location not recognized: please try again.'});
     }
   }
-
-  _onSearchTextChanged = (event) => {
-    console.log('on search text changed');
-    this.setState({ 
-      searchString: event.nativeEvent.text 
-    });
-    console.log('Current: '+ this.state.searchString+', Next: '+event.nativeEvent.text)
-  };
   
   _executeQuery = (query) => {
     fetch(query)
@@ -77,66 +78,71 @@ export default class SearchPage extends Component {
   _onSearchPressed = () => {
     const query = urlForQueryAndPage("place_name", this.state.searchString, 1);
     this._executeQuery(query);
-  };  
+  };
+  
 
-  render() {
-  const spinner = this.state.isLoading ? <ActivityIndicator size='large'/> : null;
-   return (
-     <View style={styles.container}>
-       <Text style={styles.description}>
-         Search for houses to buy!
-       </Text>
-       <Text style={styles.description}>
-         Search by place-name or postcode
-       </Text>
-       <View style={styles.flowRight}>
-         <TextInput 
-           style={styles.searchInput}
-           placeholder="Search via nanme or postcode"
-         />
-         <Button 
-           onPress={() => { }}
-           color='#48BBEC'
-           title="Go"
-         />
-       </View>
-       <Image source={require('./assets/house.png')} style={styles.image}/>
-       {spinner}
-     </View>
-   );
-  }
+  
+   render() {
+    const spinner = this.state.isLoading ? <ActivityIndicator size='large'/> : null;
+    return (
+      <View style={styles.container}>
+        <Text style={styles.description}>
+          Search for houses to buy!
+        </Text>
+        <Text style={styles.description}>
+          Search by place-name or postcode
+        </Text>
+        <View style={styles.flowRight}>
+          <TextInput 
+            style={styles.searchInput}
+            value={this.state.searchString}
+            onChange={this._onSearchTextChanged}
+            placeholder="Search via nanme or postcode"
+          />
+          <Button 
+            onPress={this._onSearchPressed}
+            color='#48BBEC'
+            title="Go"
+          />
+        </View>
+        <Image source={require('./assets/house.png')} style={styles.image}/>
+        {spinner}
+        <Text style={styles.description}>{this.state.message}</Text>
+      </View>
+    );
+   }
 }
 
 const styles = StyleSheet.create({
- description: {
-   marginBottom: 20,
-   fontSize: 18,
-   texgtAlign: 'center',
-   color: '#656565'
- },
- container: {
-   padding: 30,
-   marginTop: 65,
-   alignItems: 'center'
- },
- flowRight: {
-   flexDirection: 'row',
-   alignItems: 'center',
-   alignSelf: 'stretch'
- },
- searchInput: {
-   height: 36,
-   padding: 4,
-   marginRight: 5,
-   flexGrow: 1,
-   fontSize: 18,
-   borderWidth: 1,
-   borderColor: '#48BBEC',
-   borderRadius: 8,
-   color: '#48BBEC'
- },
- image: {
-   width: 217,
-   height: 138
- }
+  description: {
+    marginBottom: 20,
+    fontSize: 18,
+    texgtAlign: 'center',
+    color: '#656565'
+  },
+  container: {
+    padding: 30,
+    marginTop: 65,
+    alignItems: 'center'
+  },
+  flowRight: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    alignSelf: 'stretch'
+  },
+  searchInput: {
+    height: 36,
+    padding: 4,
+    marginRight: 5,
+    flexGrow: 1,
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: '#48BBEC',
+    borderRadius: 8,
+    color: '#48BBEC'
+  },
+  image: {
+    width: 217,
+    height: 138
+  }
 });
